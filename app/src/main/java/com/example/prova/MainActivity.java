943372c62564e5359003f9b3540b65f1bc772ffd
quagliot <tommaso.quaglio@ptpvenezia.edu.it>
2023-11-07 14:06:49 +0100

    continuation

--- app/src/main/java/com/example/prova/MainActivity.java
+++ app/src/main/java/com/example/prova/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.prova;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,9 +15,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText number1;
     private EditText number2;
     private TextView txtResult;
-    private Button evento;
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +26,21 @@ public class MainActivity extends AppCompatActivity {
 
         number1 = (EditText) findViewById(R.id.Numero1);
         number2 = (EditText) findViewById(R.id.Numero2);
-        txtResult =
+        txtResult = (TextView) findViewById(R.id.Risultato);
+
+
 
         final Button button = findViewById(R.id.BottoneSomma);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
-
+                String strNum1 = number1.getText().toString();
+                String strNum2 = number2.getText().toString();
+                int n1 = Integer.parseInt(strNum1);
+                int n2 = Integer.parseInt(strNum2);
+                int res = n1 + n2;
+                String strStr = String.valueOf(res);
+                txtResult.setText(strStr);
             }
         });
 
